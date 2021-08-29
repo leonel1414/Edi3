9ae68c4a32f4505225143bac0e0e5080b7adc00c
Leonel Girett <leoo9211@hotmail.com>
2021-08-28 22:55:58 -0300

    modificaciones1

--- src/app/componentes/primer-componente/primer-componente.component.ts
+++ src/app/componentes/primer-componente/primer-componente.component.ts
@@ -10,6 +10,8 @@ export class PrimerComponenteComponent implements OnInit {
   persona: Persona;
   claseDni: string = 'rojo';
   tipoControl: string = 'text';
+  nombres:Array<Persona>
+  
 
   setValue(valor:string) {
     if (valor == 'document') {
@@ -19,18 +21,31 @@ export class PrimerComponenteComponent implements OnInit {
   }
 
   constructor() {
-    this.persona = {nombre:"Leonel", apellido:"Girett", dni:36946390}
-    //this.persona.nombre = 'ingrese';
+    this.persona = { nombre: "Leonel", apellido: "Girett", dni: 36946390 }
+    this.nombres = [{ nombre: "Leonel", apellido: "Girett", dni: 36946390 },
+      { nombre: "Florencia", apellido: "Juares", dni: 36934390 },
+    {nombre:"Carlos",apellido:"Cruz",dni:369463567},];
+    
    }
 
   cambiarClase() {
     this.claseDni = 'azul';
   }
+  
   ngOnInit(): void {
   }
+  
   mostrar() {
     alert(this.persona.nombre + ' ' + this.persona.apellido + ' ' + this.persona.dni);
     this.tipoControl = 'number';
+  }
+
+  mostrarEnAlert(valor:string):void {
+    alert(valor);
+  }
+  mostrarSeleccionado() {
+    let persona = (<HTMLInputElement>document.getElementById('opPersonas')).value;
+    alert(persona);
   }
 
 }
